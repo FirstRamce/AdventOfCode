84bd2e69cee014be991170d0b436bf6b84b6ff8a
Ramon Segmüller <ramon.segmueller@abacus.ch>
2023-12-13 10:23:51 +0100

    added comment

--- year_2023/12_12_23/exercise.py
+++ year_2023/12_12_23/exercise.py
@@ -5,11 +5,11 @@ numberPattern = "\d+"
 functioningPartsPattern = "(#+)"
 class Spring:
     
-    
     def __init__(self, springConditions, controlNumbers):
         self.springConditions = springConditions
         self.controlNumbers = []
         self.validVariants = []
+        self.numValidVariants = 0
         for match in re.findall(numberPattern, controlNumbers):
             self.controlNumbers.append(int(match))
         
@@ -51,26 +51,47 @@ class Spring:
                 self.modifyNext(machineInput.replace("?","#",1))
         else:
             if self.isValidComplete(machineInput):
-                self.validVariants.append(machineInput)
+                self.numValidVariants += 1
 
     def getValidConfigurations(self):
         self.validVariants = []
         self.modifyNext(self.springConditions)
-        return self.validVariants
+        return self.numValidVariants
 
 
     
     #change from left to right (recursive) and dont continue withthe paths, that contain an illegal statement (# and . that are not possible)
+    #Nonogram solution: https://towardsdatascience.com/solving-nonograms-with-120-lines-of-code-a7c6e0f627e4
 
 springList = []
+springListFoldUp = []
+foldUpAmount = 5
 with open("input") as f:
     for line in f.readlines():
         springList.append(Spring(line.split(" ")[0],line.split(" ")[1]))
         machinePart = line.split(" ")[0]
         countingPart = line.split(" ")[1]
+        countingPart = countingPart.replace("\n","")
+        countingPart += ","
+        foldUpCountingPart = ""
+        foldUpMachinePart = ""
+        for i in range(0,foldUpAmount):
+            foldUpMachinePart += (machinePart + "?")
+            foldUpCountingPart += (countingPart)
+        foldUpMachinePart = foldUpMachinePart[:-1]
+        print(foldUpMachinePart + " " + foldUpCountingPart)
+        springListFoldUp.append(Spring(foldUpMachinePart, foldUpCountingPart))
 
 sumVariants = 0
 for spring in springList:
     validVariants = spring.getValidConfigurations()
-    sumVariants += len(validVariants)
+    sumVariants += validVariants
 print("solution 1: "+ str(sumVariants))
+
+sumVariants = 0
+for spring in springListFoldUp:
+    validVariants = spring.getValidConfigurations()
+    print(str(spring) + "solutions: " + str(validVariants))
+    sumVariants += validVariants
+    
+print("solution 2: "+ str(sumVariants))
